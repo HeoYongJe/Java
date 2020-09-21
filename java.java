9a1b58ac27fab867476e20063c635f8481b1263c
HeoYongJe <hyj.campus@gmail.com>
2020-09-21 23:44:18 +0900

    java 기초

---
+++ java.java
@@ -0,0 +1,36 @@
+public class VariableAndType{
+    
+    public static void main(string[] args) {
+    
+        short nuber1;
+        int number2;
+        long number3;
+    
+        float number4;
+        double number5;
+
+        boolean truefalse;
+
+        char writing;
+    
+        number1 = 10;
+        number2 = 9;
+        number3 = 8;
+     
+        number4 = 1.1f;
+        number5 = 2.2;
+
+        truefalse = true;
+        truefalse = false;
+
+        writing = '일';
+    
+        int a;
+        int b;
+        a = 1 + 2 + 3 + 4 + 5;
+        b = 6 + 7 + 8 + 9;
+        int result;
+        result = a + b;
+        system.out.println(result);
+    }
+}
