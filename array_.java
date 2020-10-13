7a8709b3f5877d4b925705a22d9a352d362c94c3
HeoYongJe <hyj.campus@gmail.com>
2020-10-14 02:31:02 +0900

    array 간단 실습

---
+++ array_.java
@@ -0,0 +1,26 @@
+public class Array_ {
+
+    public class Array_ {
+    
+        public static void main(string[] args) {
+
+            // int a;
+            // Integer a ;
+
+            // boolean b;
+            // Boolean b2;
+
+            // long c;
+            // Long c2;
+
+            // Long item;
+            // item = null; //item 이 존재하지 않게된다 , 다른곳에서 사용 x Long 안에있는 값을 알 수 없음
+
+            // Primitive type 에서는 null 들어갈 수 없고 / Reference type에는 null이 들어갈 수 있다
+            ArrayList<Integer> ints = new ArrayList();
+
+        }
+    
+    }
+
+}
